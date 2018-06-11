0a97b90dc4911af6da093e9fec233afda0f5aef6
Shoul Manevitz <shoulma@gmail.com>
2018-06-11 15:36:37 +0300

    Add files via upload

---
+++ primo-studio-addon-tutorial/js/appStoreFeatureDemo.js
@@ -0,0 +1,28 @@
+
+/*in order to access the parameters configured when installing this addon 
+we inject a service called npm-id in camel case concated to 'StudioConfig'*/
+app.controller('primoStudioAddonTutorialController', ['primoStudioAddonTutorialStudioConfig',function (addonParameters) {
+	
+    var vm = this;
+	
+	vm.configuredText = getConfiguredText();
+	
+	function getConfiguredText(){
+		return addonParameters.text;
+	}
+
+}]);
+
+
+
+//the name of the component must be the npm-id in camel case
+app.component('primoStudioAddonTutorial', {
+    bindings: {parentCtrl: '<'},
+    controller: 'primoStudioAddonTutorialController',
+    template: `
+            <div layout="row" layout-align="center center">
+				<span>{{$ctrl.configuredText}}</span>
+             </div>
+`
+
+});
